87db58a03086ca009abfda223082c8a3d97667cf
Tom Meagher <thom.meagher@gmail.com>
2018-06-02 17:15:22 -0400

    Add nuclear mode switch to localStorage, remove site from options

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,4 @@
 const blockedSitesKey = 'bf_sites';
 const blockedSiteKey = 'bf_site';
 const enabledSwitchKey = 'bf_enabled';
+const nuclearSwitchKey = 'bf_nuclear';
--- src/options/index.js
+++ src/options/index.js
@@ -1,40 +1,61 @@
 // CONSTANTS
 const blockedSitesListId = '#blocked-sites';
 const enabledSwitchId = '#enabled-switch';
+const nuclearSwitchId = '#nuclear-switch';
+const addContainerClass = '.options__sites-add'
 // CONSTANTS
 
 // INIT
+chrome.tabs.getSelected(null, (tab) => {
+    if (`chrome-extension://${chrome.runtime.id}/src/options/index.html` === tab.url) {
+        console.log('options page');
+    }
+});
+
 let sites = JSON.parse(localStorage.getItem(blockedSitesKey)) || [];
 const blockedSitesListElement = document.querySelector(blockedSitesListId);
-sites.forEach((site) => {
+sites.forEach((site, index) => {
     const prettyUrl = getPrettyUrl(site);
     const markup = `
-        <div id="${site}" class="options__site">
+        <div id="site-${index}" class="options__site">
+            <button id="delete-${index}" class="options__site-delete" data-site="${site}"></button>
             <a href="${site}">${prettyUrl}</a>
         </div>
     `;
     blockedSitesListElement.insertAdjacentHTML('afterend', markup);
+    const deleteElement = document.querySelector(`#delete-${index}`);
+    deleteElement.addEventListener('click', () => {
+        sites = sites.filter(site => site !== deleteElement.dataset.site);
+        localStorage.setItem(blockedSitesKey, JSON.stringify(sites));
+        const siteElement = document.querySelector(`#site-${index}`);
+        siteElement.parentNode.removeChild(siteElement);
+    });
 });
 
 const enabled = localStorage.getItem(enabledSwitchKey) !== null &&
                 localStorage.getItem(enabledSwitchKey) === 'true';
 updateSwitchForId(enabledSwitchId, enabled);
+
+const nuclearEnabled = localStorage.getItem(nuclearSwitchKey) !== null &&
+    localStorage.getItem(nuclearSwitchKey) === 'true';
+updateSwitchForId(nuclearSwitchId, nuclearEnabled);
 // INIT
 
 // EVENT LISTENERS
 const enabledSwitchElement = document.querySelector(enabledSwitchId);
 enabledSwitchElement.addEventListener('change', () =>
     localStorage.setItem(enabledSwitchKey, enabledSwitchElement.checked));
-// const siteSwitchElement = document.querySelector(siteSwitchId);
-// siteSwitchElement.addEventListener('change', () => {
-//     chrome.tabs.getSelected(null, (tab) => {
-//         const url = getUrlForTab(tab);
-//         if (siteSwitchElement.checked) {
-//             sites.push(url)
-//         } else {
-//             sites = sites.filter(site => site !== url);
-//         }
-//         localStorage.setItem(blockedSitesKey, JSON.stringify(sites));
-//     });
-// });
+
+const nuclearSwitchElement = document.querySelector(nuclearSwitchId);
+nuclearSwitchElement.addEventListener('change', () =>
+    localStorage.setItem(nuclearSwitchKey, nuclearSwitchElement.checked));
+
+const addContainerElement = document.querySelector(addContainerClass);
+const addInputElement = addContainerElement.getElementsByTagName('input')[0];
+const addButtonElement = addContainerElement.getElementsByTagName('button')[0];
+addButtonElement.addEventListener('click', () => {
+    if (addInputElement.value && addInputElement.value.length) {
+        console.log('addButtonElement', addInputElement.value);
+    }
+});
 // EVENT LISTENERS
